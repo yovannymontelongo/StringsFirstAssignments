35e8cfe40e0a45a829a479d2ca2d28305c21c3b2
Yovanny Montelongo <yova.monte.2017@gmail.com>
2022-04-02 21:00:41 -0500

    INITIAL COMMIT

---
+++ Part1.java
@@ -0,0 +1,13 @@
+import edu.duke.*;
+
+public class Part1{
+    public static void main(String[] args){
+
+    }
+
+    public static String findSimpleGene(String dna){
+        String subdna = "";
+
+        return subdna;
+    }
+}
